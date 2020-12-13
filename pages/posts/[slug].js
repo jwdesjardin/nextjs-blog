f9892822d92a96c5113677dfbedc3b96f1a9c9c6
jwdesjardin <63127698+jwdesjardin@users.noreply.github.com>
2020-12-12 16:35:58 -0800

    pre-deployment
    Built following all the steps of the next.js tutorial for static site generated blog

---
+++ pages/posts/[slug].js
@@ -0,0 +1,41 @@
+import Layout from '../../components/layout';
+import { getAllPostIds, getPostData } from '../../lib/posts';
+import Head from 'next/head';
+import Date from '../../components/date';
+import utilStyles from '../../styles/utils.module.css';
+
+//sets props in Post Component - gets params from fileName
+export async function getStaticProps({ params }) {
+	const postData = await getPostData(params.slug);
+	return {
+		props: {
+			postData
+		}
+	};
+}
+
+//creates paths
+export async function getStaticPaths() {
+	const paths = getAllPostIds();
+	return {
+		paths,
+		fallback: false
+	};
+}
+
+export default function Post({ postData }) {
+	return (
+		<Layout>
+			<Head>
+				<title>{postData.title}</title>
+			</Head>
+			<article>
+				<h1 className={utilStyles.headingXl}>{postData.title}</h1>
+				<div className={utilStyles.lightText}>
+					<Date dateString={postData.date} />
+				</div>
+				<div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+			</article>
+		</Layout>
+	);
+}
